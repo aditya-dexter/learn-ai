f2d31976cbdca5f2a18c4ac193254423d456900f
Dexter <ankush@MacBook-Pro.mynet>
2025-09-17 23:44:45 +0100

    refactoring for project structure

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import os
 
 from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_ollama import ChatOllama
 
 load_dotenv()
 
@@ -9,6 +12,37 @@ def main():
     print("Hello from learn-ai!")
     print(os.getenv("GOOGLE_API_KEY"))
 
+    information = """
+    Aditya is a helpful assistant that can answer questions and help with tasks. 
+    He is a student of Computer Science and Engineering at the University of Delhi.
+    He is a good student and he is a good person.
+    """
+
+    summary_template = """
+        {information}
+        Please summarize the information in a one sentence.
+    """
+
+    prompt = PromptTemplate(
+        template=summary_template,
+        input_variables=["information"]
+    )
+
+    llm = ChatGoogleGenerativeAI(
+        model="gemini-2.0-flash-001",
+        api_key=os.getenv("GOOGLE_API_KEY"),
+        temperature=0.0
+    )
+
+    llmOllama = ChatOllama(
+        model="gemma3:latest",
+        temperature=0.0
+    )
+
+    chain = prompt | llmOllama
+
+    result = chain.invoke({"information": information})
+    print(result.content)
 
 if __name__ == "__main__":
     main()
